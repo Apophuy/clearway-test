2912978442722bad12efc9e0d1c0a4e23bab1219
Apophuy <[apophuy@hotmail.com]>
2025-03-04 13:16:52 +0300

    added modal props

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,10 +1,12 @@
 import { TextFieldModule } from '@angular/cdk/text-field';
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {
+  MAT_DIALOG_DATA,
   MatDialogActions,
   MatDialogClose,
   MatDialogContent,
+  MatDialogRef,
   MatDialogTitle,
 } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
@@ -25,40 +27,37 @@ import { DataService } from '../../services/data.service';
     MatInputModule,
     TextFieldModule,
   ],
+  providers: [DataService],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss',
 })
 export class ModalComponent {
-  constructor(public dataService: DataService) {}
+  isImage = false;
+  isText = false;
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public dialogData: { docId: number; pageId: number; getAnnId: () => string },
+    private dialogRef: MatDialogRef<ModalComponent>,
+  ) {}
 
   onImportImageSelect(event: Event) {
     const input = event.target as HTMLInputElement;
     if (!input.files?.length) {
       return;
     }
-
     const imageUrl = URL.createObjectURL(input.files[0]);
-    this.dataService.isImage = true;
-    console.log(
-      'currentDocId: ',
-      this.dataService.currentDocId,
-      'currentPageId: ',
-      this.dataService.currentPageId,
-    );
-
-    const imageKey = this.dataService.getAnnId();
-    console.log('imageKey: ', imageKey);
+    this.isImage = true;
+    const imageKey = this.dialogData.getAnnId();
   }
 
   onTextEnter(event: Event) {
     const inputText = (event.target as HTMLInputElement).value;
-    const textKey = this.dataService.getAnnId();
-    this.dataService.isText = true;
+    const textKey = this.dialogData.getAnnId();
+    this.isText = true;
   }
 
   onCancel() {
-    this.dataService.isImage = false;
-    this.dataService.isText = false;
-    this.dataService.currentPageId = null;
+    this.isImage = false;
+    this.isText = false;
   }
 }
